1441c316ad480061f16839dfc94dcc0f64cfafc6
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-18 13:05:10 -0700

    completed methods for melon types part one

--- harvest.py
+++ harvest.py
@@ -9,6 +9,13 @@ class MelonType(object):
     def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
                  name):
         """Initialize a melon."""
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
+
 
         self.pairings = []
 
@@ -18,10 +25,11 @@ class MelonType(object):
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
+        self.pairings.append(pairing)
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
-
+        self.code = new_code
         # Fill in the rest
 
 
@@ -31,18 +39,39 @@ def make_melon_types():
     all_melon_types = []
 
     # Fill in the rest
+    muskemelon = MelonType("musk", 1988, "green", True, True, "Muskmelon")
+    all_melon_types.append(muskemelon)
+    muskemelon.add_pairing("mint")
+
+
+    casaba = MelonType("cas",2003,"orange",False, False, "Casaba")
+    all_melon_types.append(casaba)
+    casaba.add_pairing("strawberries")
+    casaba.add_pairing("mint")
+
+    crenshaw = MelonType("cren",1996, "green", True ,False, "Crenshaw")
+    all_melon_types.append(crenshaw)
+    crenshaw.add_pairing("proscuitto")
 
     return all_melon_types
 
+
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
-
     # Fill in the rest
+    for melon in melon_types:
+        print(f"{melon.name} pairs with")
+        for pair in melon.pairings:
+            print(f"- {pair}")
+
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
-
+    melon_codes = {}
+    for melon in melon_types:
+        melon_codes[melon.code] = melon
     # Fill in the rest
+    return melon_codes
 
 ############
 # Part 2   #
